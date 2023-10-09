044464cbefc895f8bc661fdfaac9917eecbd3dbb
ujjwalrastogi-17 <140800905+ujjwalrastogi-17@users.noreply.github.com>
2023-10-09 18:26:06 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
     <div className="flex flex-col gap-[20px]">
       <ComponentNavbar />
       <ComponentLowerNav />
+      Ujjwal
       <ComponentCategories />
       <RouteManager />
       <ComponentFooter />
